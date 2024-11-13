af9b18b6ab17c14fbb007d28fa5b12fbaccdf077
diorm47 <dior47m@gmail.com>
2024-11-14 01:41:32 +0500

    aos

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./app.css";
+import { ReactComponent as Arrow } from "./assets/arrow.svg";
+import header_img from "./assets/header-img.png";
 import MainBtn from "./components/main-btn/main-btn";
 import Navbar from "./components/navbar/navbar";
-import header_img from "./assets/header-img.png";
-import { ReactComponent as Arrow } from "./assets/arrow.svg";
 
 import ins_img_1 from "./assets/ins-imgs/img1.png";
 import ins_img_2 from "./assets/ins-imgs/img2.png";
@@ -15,13 +15,23 @@ import scx_img_1 from "./assets/scan-x/img1.png";
 import scx_img_2 from "./assets/scan-x/img2.png";
 import scx_img_3 from "./assets/scan-x/img3.png";
 
+import AOS from "aos";
 import roadm from "./assets/roadmap-img1.png";
+
 import widgetImg from "./assets/widget-img.png";
 
+import "aos/dist/aos.css";
+import { useEffect, useRef } from "react";
 import qr from "./assets/qr.png";
-import { useRef } from "react";
 
 function App() {
+
+  useEffect(() => {
+    AOS.init({
+      duration: 1700,
+    });
+  }, []);
+
   const sectionRefs = {
     section1: useRef(null),
     section2: useRef(null),
@@ -39,32 +49,32 @@ function App() {
 
       <section>
         <div className="main_header">
-          <h1>
+          <h1 data-aos="fade-up">
             Эффективный <span>Telegram</span> скринер криптовалют
           </h1>
-          <p>
+          <p data-aos="fade-up">
             Делаем анализ криптовалюты более эффективным <br />и находим сигналы
             для успешных инвестиций.
           </p>
-          <div className="dfaic">
+          <div className="dfaic" data-aos="fade-up">
             <MainBtn text="Начать сейчас" />
           </div>
-          <div className="dfaic">
+          <div className="dfaic" data-aos="fade-up">
             <img className="header_img" src={header_img} alt="" />
           </div>
         </div>
       </section>
 
       <section ref={sectionRefs.section1}>
-        <div className="section_title">
-          <h2>Инструменты Scan -x</h2>
-          <p>
+        <div className="section_title"  >
+          <h2 data-aos="fade-right">Инструменты Scan -x</h2>
+          <p data-aos="fade-left">
             Позволяют одним нажатием отбирать монеты, которые могут быть <br />
             интересны для внутридневной торговли.
           </p>
         </div>
         <div className="instr_cards">
-          <div className="inst_card">
+          <div className="inst_card"  data-aos="fade-right">
             <div className="inst_card_title">
               <div>
                 <h4 className="w26px">Топ лидеров роста за 24 часа</h4>
@@ -77,7 +87,7 @@ function App() {
             </div>
             <img className="inst_card_img" src={ins_img_1} alt="" />
           </div>
-          <div className="inst_card inst_card_s">
+          <div className="inst_card inst_card_s"  data-aos="fade-up">
             <div className="inst_card_title">
               <div>
                 <h4>Топ лидеров падения за 24 часа</h4>
@@ -90,7 +100,7 @@ function App() {
             </div>
             <img className="inst_card_img" src={ins_img_2} alt="" />
           </div>
-          <div className="inst_card">
+          <div className="inst_card"  data-aos="fade-left">
             <div className="inst_card_title">
               <div>
                 <h4 className="secondary_t">
@@ -105,7 +115,7 @@ function App() {
             </div>
             <img className="inst_card_img" src={ins_img_3} alt="" />
           </div>
-          <div className="inst_card inst_card_s">
+          <div className="inst_card inst_card_s" data-aos="fade-right">
             <div className="inst_card_title">
               <div>
                 <h4>Самые волатильные монеты за 24 часа</h4>
@@ -118,7 +128,7 @@ function App() {
             </div>
             <img className="inst_card_img" src={ins_img_4} alt="" />
           </div>
-          <div className="inst_card">
+          <div className="inst_card"   data-aos="fade-up">
             <div className="inst_card_title">
               <div>
                 <h4>Монеты вблизи значений ATH</h4>
@@ -131,7 +141,7 @@ function App() {
             </div>
             <img className="inst_card_img" src={ins_img_5} alt="" />
           </div>
-          <div className="inst_card inst_card_s">
+          <div className="inst_card inst_card_s"  data-aos="fade-left">
             <div className="inst_card_title">
               <div>
                 <h4>Монеты вблизи значений ATL</h4>
@@ -148,15 +158,15 @@ function App() {
       </section>
 
       <section ref={sectionRefs.section2}>
-        <div className="section_title">
-          <h2>Сигналы от Scan-x</h2>
-          <p>
+        <div className="section_title"  >
+          <h2 data-aos="fade-right">Сигналы от Scan-x</h2>
+          <p data-aos="fade-left">
             Получайте мгновенно актуальные торговые сигналы <br /> и
             рекомендации по криптовалютам
           </p>
         </div>
         <div className="scanx_cards">
-          <div className="scanx_card">
+          <div className="scanx_card" data-aos="fade-right">
             <h4>Сигналы Scan-x</h4>
             <img src={scx_img_1} alt="" />
             <p>
@@ -164,7 +174,7 @@ function App() {
               изменения.
             </p>
           </div>
-          <div className="scanx_card">
+          <div className="scanx_card" data-aos="fade-up">
             <h4>Рекомендации от Scan-x</h4>
             <img src={scx_img_2} alt="" />
             <p>
@@ -174,7 +184,7 @@ function App() {
               торгов в криптовалюте.
             </p>
           </div>
-          <div className="scanx_card">
+          <div className="scanx_card" data-aos="fade-left">
             <h4>Новости</h4>
             <img src={scx_img_3} alt="" />
             <p>
@@ -187,12 +197,12 @@ function App() {
       </section>
 
       <section ref={sectionRefs.section3}>
-        <div className="section_title">
+        <div className="section_title"  data-aos="fade-left">
           <h2>Дорожная карта</h2>
         </div>
         <div className="roadmap_wrapper">
           <div className="roadmap_left">
-            <div className="roadmap_card roadmap_card_sec">
+            <div className="roadmap_card roadmap_card_sec"  data-aos="fade-right">
               <div>
                 <h5>Скоро</h5>
                 <h3>Mini apps Scan-x</h3>
@@ -205,7 +215,7 @@ function App() {
 
               <img src={roadm} alt="" />
             </div>
-            <div className="roadmap_card mb0">
+            <div className="roadmap_card mb0"  data-aos="fade-right">
               <h5>Скоро</h5>
               <h3>Веб терминал Scan-x</h3>
               <p>
@@ -217,7 +227,7 @@ function App() {
             </div>
           </div>
           <div className="roadmap_right">
-            <div className="roadmap_card">
+            <div className="roadmap_card"  data-aos="fade-left">
               <h5>Скоро</h5>
               <h3>Автоматическая торговля</h3>
               <p>
@@ -227,7 +237,7 @@ function App() {
                 торгов, а также тренды, такие как памп или дамп.
               </p>
             </div>
-            <div className="roadmap_card mb0">
+            <div className="roadmap_card mb0"  data-aos="fade-left">
               <h5>Скоро</h5>
               <h3>Настраиваемые алерты</h3>
               <p>
@@ -243,17 +253,17 @@ function App() {
       </section>
 
       <section>
-        <div className="widget">
-          <div className="widget_text">
+        <div className="widget" data-aos="fade-up">
+          <div className="widget_text" data-aos="fade-right">
             <p>
               Используйте <br /> Scan-x <span>чтобы быть на шаг впереди.</span>
             </p>
             <MainBtn text="Начать сейчас" />
           </div>
-          <div className="widget_img">
+          <div className="widget_img" data-aos="fade-up">
             <img src={widgetImg} />
           </div>
-          <div className="qr">
+          <div className="qr" data-aos="fade-left">
             <img src={qr} alt="" />
             <p>
               Отсканируйте QR-код для <br />
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,22 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./navbar.css";
 import MainBtn from "../main-btn/main-btn";
 import { ReactComponent as Logo } from "../../assets/logo.svg";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function Navbar({ handleScroll }) {
+  useEffect(() => {
+    AOS.init({
+      duration: 1700,
+    });
+  }, []);
+
   return (
     <nav>
-      <div className="nav_logo">
+      <div className="nav_logo" data-aos="fade-right">
         <Logo />
         <h1>Scan-x</h1>
       </div>
 
-      <div className="nav_menu">
+      <div className="nav_menu" data-aos="fade-down">
         <p onClick={() => handleScroll("section1")}>Инструменты</p>
         <p onClick={() => handleScroll("section2")}>Возможности</p>
         <p onClick={() => handleScroll("section3")}>Дорожная карта</p>
       </div>
-      <MainBtn text="Попробовать" />
+      <div data-aos="fade-left">
+        <MainBtn text="Попробовать" />
+      </div>
     </nav>
   );
 }
