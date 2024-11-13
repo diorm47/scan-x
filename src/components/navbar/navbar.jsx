e231d47769ab770c2ae0ef02e601c8585e2707eb
diorm47 <dior47m@gmail.com>
2024-11-14 01:22:49 +0500

    favicon

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./navbar.css";
 import MainBtn from "../main-btn/main-btn";
+import { ReactComponent as Logo } from "../../assets/logo.svg";
 
-function Navbar({handleScroll}) {
+function Navbar({ handleScroll }) {
   return (
     <nav>
       <div className="nav_logo">
+        <Logo />
         <h1>Scan-x</h1>
       </div>
 
@@ -14,7 +16,7 @@ function Navbar({handleScroll}) {
         <p onClick={() => handleScroll("section2")}>Возможности</p>
         <p onClick={() => handleScroll("section3")}>Дорожная карта</p>
       </div>
-      <MainBtn text='Попробовать' />
+      <MainBtn text="Попробовать" />
     </nav>
   );
 }
